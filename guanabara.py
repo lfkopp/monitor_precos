6bfea19bef10de01d79b03932d87d46dc0d1a10c
Lfkopp <lfkopp@gmail.com>
2024-09-12 22:50:57 -0300

    guanabara

--- guanabara.py
+++ guanabara.py
@@ -1,11 +1,25 @@
+#%%
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+import datetime
+hoje = datetime.datetime.now().strftime('%Y-%m-%d')
+#%%
 url_principal = 'https://www.supermercadosguanabara.com.br/produtos'
 pagina_principal = requests.get(url_principal)
-soup = BeautifulSoup(pagina_principal.content)
-for link in soup.find_all('a'):
-    if '/produtos/' in link['href']:
-        pagina = requests.get('https://www.supermercadosguanabara.com.br/'+link['href'])
-        soup = BeautifulSoup(pagina.content)
-        for item in soup.find_all('div', {'class':'col item'}):
-            print(item.find('div',{'class':'name'}).text,';',item.find('span',{'class':'number'}).text )
+soup = BeautifulSoup(pagina_principal.content, 'lxml')
+secoes = [(x['href'], x.text.strip()) for x in soup.find('div', {'class':'products-menu menu-bar'}).find_all('a')]
+print('numero de secoes: ',len(secoes))
+#%%
+with open('guanabara.txt', 'a+', encoding='utf-8') as f:
+    for link,secao in secoes:
+        pagina = requests.get('https://www.supermercadosguanabara.com.br'+link)
+        soup = BeautifulSoup(pagina.content, 'lxml')
+        itens = soup.find_all('div', {'class':'col item'})
+        print(secao,len(itens))
+        for item in itens:
+            produto = item.find('div',{'class':'name'}).text
+            preco = item.find('span',{'class':'number'}).text
+            f.write(f'{hoje};{link};{secao};{produto};{preco}\n')
+        sleep(2.3)
+# %%
